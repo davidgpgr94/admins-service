ffc327cca03365fa217522e115bb81db3475ac05
davidgpgr94 <davidgpgr94@gmail.com>
2022-01-23 20:59:22 +0100

    refactor(Root): Use awilix loadModules to load all use cases

--- src/apps/rest-api/container.js
+++ src/apps/rest-api/container.js
@@ -1,21 +1,10 @@
 const awilix = require('awilix');
+const path = require('path');
 
 const WinstonLogger = require('../../contexts/shared/infrastructure/logger/winston-logger');
 
 // Domain services/helpers
 const AdminSensitiveFieldsFilter = require('../../contexts/admin/domain/services/admin-sensitive-fields-filter');
-
-// Use Cases
-const CheckCredentialsUseCase = require('../../contexts/admin/application/check-credentials/check-credentials.use-case');
-const CreateAdminUseCase = require('../../contexts/admin/application/create-admin/create-admin.use-case');
-const GetAdminsUseCase = require('../../contexts/admin/application/get-admins/get-admins.use-case');
-const GetAdminUseCase = require('../../contexts/admin/application/get-admin/get-admin.use-case');
-const EnableAdminUseCase = require('../../contexts/admin/application/enable-admin/enable-admin.use-case');
-const DisableAdminUseCase = require('../../contexts/admin/application/disable-admin/disable-admin.use-case');
-const EditAdminUseCase = require('../../contexts/admin/application/edit-admin/edit-admin.use-case');
-const ChangePasswordUseCase = require('../../contexts/admin/application/change-password/change-password.use-case');
-const RequestRestorePasswordUseCase = require('../../contexts/admin/application/request-restore-password/request-restore-password.use-case');
-const RestorePasswordUseCase = require('../../contexts/admin/application/restore-password/restore-password.use-case');
 
 const SqliteAdminRepository = require('../../contexts/admin/infrastructure/services/sqlite-admin-repository');
 const sqliteDbHandler = require('../../contexts/shared/infrastructure/persistence/sqlite-db-handler');
@@ -31,18 +20,6 @@ async function createContainer() {
   container.register({
     logger: awilix.asClass(WinstonLogger).singleton(),
 
-    // use cases
-    checkCredentialsUseCase: awilix.asClass(CheckCredentialsUseCase),
-    createAdminUseCase: awilix.asClass(CreateAdminUseCase),
-    getAdminsUseCase: awilix.asClass(GetAdminsUseCase),
-    getAdminUseCase: awilix.asClass(GetAdminUseCase),
-    enableAdminUseCase: awilix.asClass(EnableAdminUseCase),
-    disableAdminUseCase: awilix.asClass(DisableAdminUseCase),
-    editAdminUseCase: awilix.asClass(EditAdminUseCase),
-    changePasswordUseCase: awilix.asClass(ChangePasswordUseCase),
-    requestRestorePasswordUseCase: awilix.asClass(RequestRestorePasswordUseCase),
-    restorePasswordUseCase: awilix.asClass(RestorePasswordUseCase),
-
     // admin-services
     passwordEncryptor: awilix.asClass(BcryptPasswordEncryptor),
     adminSensitiveFieldsFilter: awilix.asClass(AdminSensitiveFieldsFilter),
@@ -56,6 +33,21 @@ async function createContainer() {
     sqliteDbHandler: awilix.asFunction(sqliteDbHandler).singleton(),
   });
 
+  // Use cases
+  container.loadModules(
+    [
+      './**/application/**/*.use-case.js'
+    ],
+    {
+      cwd: path.join(__dirname, '..', '..', 'contexts'),
+      formatName: 'camelCase',
+      resolverOptions: {
+        register: awilix.asClass
+      }
+    }
+  );
+
+  // Api controllers and serializers
   container.loadModules(
     [
       './controllers/**/*.controller.js',
